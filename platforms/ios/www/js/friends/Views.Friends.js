09de5fc20281dc328bfdb568b63b5d472a621161
Puneeth Rai <npuneethrai@yahoo.co.in>
2014-10-19 00:11:07 +0530

    SCSS and UI alignment
    Segregated SCSS files and UI alignment changes

--- platforms/ios/www/js/friends/Views.Friends.js
+++ platforms/ios/www/js/friends/Views.Friends.js
@@ -1,52 +1,56 @@
 var ViewFriends = Backbone.View.extend({
-    initialize: function initialize (options) {
+    id: "Friends",
+    initialize: function initialize(options) {
         this.options = options;
-        this.template = templates.get('friend','Friends');
+        this.template = templates.get('friend', 'Friends');
         this.collection.on({
-            "add" : this.onNewFriend,
-            "remove" : this.onDeleteFriend
+            "add": this.onNewFriend,
+            "remove": this.onDeleteFriend
         }, this);
-        this.tAmount = 0;
-        this.yAmount = 0
     },
-    render : function render () {
+    render: function render() {
+        var modelIndex = 0;
         this.$el.html(this.template({
             friends: this.collection.models.length
-        })); 
+        }));
         $("#" + this.options.parentDiv).append(this.$el);
-        for(var modelIndex = 0; modelIndex < this.collection.models.length; modelIndex++){
+        for (modelIndex = 0; modelIndex < this.collection.models.length; modelIndex++) {
             this._createFriendsView(this.collection.models[modelIndex]);
         }
         return this;
     },
     events: {
-        "tap .dummyAddFriends"                : "onAddFriend",
+        "tap .dummyAddFriends": "onAddFriend",
     },
 
-    onNewFriend: function onNewFriend (model , collection) {
-        this._createFriendsView(model,{validate:true});
+    onNewFriend: function onNewFriend(model, collection) {
+        /*jslint unparam:true*/
+        this._createFriendsView(model, {
+            validate: true
+        });
         this.$el.find(".dummyFriendsCount").html(this.collection.models.length);
-        if(model.isNew()){
+        if (model.isNew()) {
             model.save();
-        
+
         }
 
     },
-    onAddFriend: function onAddCredit () {
+    onAddFriend: function onAddCredit() {
         this._onAddToCollection(this.$el.find(".dummyFriendName").val());
     },
-    _onAddToCollection : function _onAddToCollection (name) {
+    _onAddToCollection: function _onAddToCollection(name) {
         this.collection.create({
             name: name
-        },{ 
+        }, {
             validate: true,
-            wait:true
+            wait: true
         });
     },
-    onDeleteFriend: function onDeleteTransaction (model) {
+    onDeleteFriend: function onDeleteTransaction(model) {
+        /*jslint unparam:true*/
         this.$el.find(".dummyFriendsCount").html(this.collection.models.length);
     },
-    _createFriendsView: function _createFriendsView (model) {
+    _createFriendsView: function _createFriendsView(model) {
         var FriendView = new ViewFriend({
             model: model
         }).render();
